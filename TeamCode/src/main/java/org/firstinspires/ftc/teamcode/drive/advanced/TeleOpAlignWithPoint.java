40da863048410ac4f7a30312a4919a60918c63a4
Noah Bresler <noahbresler2002@gmail.com>
2020-10-16 16:49:19 -0500

    Add dashboard drawing to TeleOpAlignWithPoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/TeleOpAlignWithPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/TeleOpAlignWithPoint.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.drive.advanced;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.control.PIDFController;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -10,18 +13,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.util.DashboardUtil;
 
 /**
- * This opmode demonstrates how one would implement "align to point behavior" in teleop. This opmode
- * allows you to specify a `targetPosition`. In the `ALIGN_TO_POINT` mode, the bot will switch into
- * field centric control and independently control its heading to align itself with the specified
- * `targetPosition`.
+ * This opmode demonstrates how one would implement "align to point behavior" in teleop. You specify
+ * a desired vector (x/y coordinate) via`targetPosition`. In the `ALIGN_TO_POINT` mode, the bot will
+ * switch into field centric control and independently control its heading to align itself with the
+ * specified `targetPosition`.
  * <p>
  * Press `a` to switch into alignment mode and `b` to switch back into standard teleop driving mode.
+ * <p>
+ * Note: We don't call drive.update() here because it has its own field drawing functions. We don't
+ * want that to interfere with our graph so we just directly update localizer instead
  */
 @Config
 @TeleOp(group = "advanced")
 public class TeleOpAlignWithPoint extends LinearOpMode {
+
+    public static double DRAWING_TARGET_RADIUS = 2;
 
     // Define 2 states, driver control or alignment control
     enum Mode {
@@ -50,7 +59,7 @@ public class TeleOpAlignWithPoint extends LinearOpMode {
 
         // Retrieve our pose from the PoseStorage.currentPose static field
         // See AutoTransferPose.java for further details
-        drive.setPoseEstimate(PoseStorage.currentPose);
+        drive.getLocalizer().setPoseEstimate(PoseStorage.currentPose);
 
         // Set input bounds for the heading controller
         // Automatically handles overflow
@@ -62,13 +71,17 @@ public class TeleOpAlignWithPoint extends LinearOpMode {
 
         while (opModeIsActive() && !isStopRequested()) {
             // Read pose
-            Pose2d poseEstimate = drive.getPoseEstimate();
+            Pose2d poseEstimate = drive.getLocalizer().getPoseEstimate();
 
             // Declare a drive direction
             // Pose representing desired x, y, and angular velocity
             Pose2d driveDirection = new Pose2d();
 
             telemetry.addData("mode", currentMode);
+
+            // Declare telemetry packet for dashboard field drawing
+            TelemetryPacket packet = new TelemetryPacket();
+            Canvas fieldOverlay = packet.fieldOverlay();
 
             switch (currentMode) {
                 case NORMAL_CONTROL:
@@ -121,15 +134,34 @@ public class TeleOpAlignWithPoint extends LinearOpMode {
                             robotFrameInput,
                             headingInput
                     );
+
+                    // Draw the target on the field
+                    fieldOverlay.setStroke("#dd2c00");
+                    fieldOverlay.strokeCircle(targetPosition.getX(), targetPosition.getY(), DRAWING_TARGET_RADIUS);
+
+                    // Draw lines to target
+                    fieldOverlay.setStroke("#b89eff");
+                    fieldOverlay.strokeLine(targetPosition.getX(), targetPosition.getY(), poseEstimate.getX(), poseEstimate.getY());
+                    fieldOverlay.setStroke("#ffce7a");
+                    fieldOverlay.strokeLine(targetPosition.getX(), targetPosition.getY(), targetPosition.getX(), poseEstimate.getY());
+                    fieldOverlay.strokeLine(targetPosition.getX(), poseEstimate.getY(), poseEstimate.getX(), poseEstimate.getY());
                     break;
             }
+
+            // Draw bot on canvas
+            fieldOverlay.setStroke("#3F51B5");
+            DashboardUtil.drawRobot(fieldOverlay, poseEstimate);
 
             drive.setWeightedDrivePower(driveDirection);
 
             // Update the heading controller with our current heading
             headingController.update(poseEstimate.getHeading());
-            // Update the drive class
-            drive.update();
+
+            // Update he localizer
+            drive.getLocalizer().update();
+
+            // Send telemetry packet off to dashboard
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
 
             // Print pose to telemetry
             telemetry.addData("x", poseEstimate.getX());
